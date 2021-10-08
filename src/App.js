a7334889d38502cf281ce533d811cd5392c009b0
jamiegallagher <G00377566@gmit.ie>
2021-10-08 21:52:17 +0100

    second lab complete

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
 import logo from './logo.svg';
 import './App.css';
+import { Component } from 'react';
+import { Header } from './components/header';
+import { Footer } from './components/footer';
+import { Content } from './components/contents';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Navbar, Nav } from 'react-bootstrap';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  render() {
+    return (
+      //outputting Hello World and The current time to the screen
+      //We also use the header, content and footer taga 
+      //Which are declared in the header.js, contents.js and footer.js files to export the contents of the file 
+      //And output onto the screen of App.js
+      <Router>
+        <div className="App">
+          <Navbar bg="primary" variant="dark">
+            <Navbar.Brand href="#home">Navbar</Navbar.Brand>
+            <Nav className="me-auto">
+              <Nav.Link href="/">Home</Nav.Link>
+              <Nav.Link href="/read">Read</Nav.Link>
+              <Nav.Link href="/create">Create</Nav.Link>
+            </Nav>
+          </Navbar>
+
+          <br />
+          <Switch>
+          <Route path='/' component={Content} exact/>
+          <Route path='/create' component={Header} ecact />
+          <Route path='/read' component={Footer} exact/>
+          </Switch>
+        </div>
+      </Router>
+    );
+  }
 }
 
 export default App;
